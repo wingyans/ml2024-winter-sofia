001355cb61a8b5fc2c9d36dd4bf10bbf9dac9b7b
wingyans <168396853+wingyans@users.noreply.github.com>
2024-04-28 22:52:06 -0700

    Update module4.py

--- module4.py
+++ module4.py
@@ -1,6 +1,15 @@
-def user_input():
+#prompt:
+#The program asks the user for input N (positive integer) and reads it
+#Then the program asks the user to provide N numbers (one by one) and reads all of them (again, one by one)
+#In the end, the program asks the user for input X (integer) and outputs: "-1" if there were no such X among N read numbers, or the index (from 1 to N) of this X if the user input it before.
 
-	user_input = input("Please type in your name\n")
 
-	# Printing a message based on the input.
-	print(f"Welcome, {user_input}!")
+#solution:
+#(1) program ask users to provide a positive integer N (used as total number of items that will be stored in the list)
+#(2) create a blank list that can hold N number of items 
+#(3) use a loop to ask user to enter the N numbers, one number each time (the number list will be updated with each number provided)
+#(4) ask the user to provide target value X which would be compared against the list of numbers that was provided earlier
+#(5) a for loop should be used in the search function to compare X against each entry in the list one by one
+#(6) if a match is identified between X and the list, the match item's index position should be returned from the function 
+#(7) if no match is found between X and the list, the function will return -1
+
