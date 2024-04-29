b82d45417b685e47063eea5c2e188c1eaa0085b3
wingyans <168396853+wingyans@users.noreply.github.com>
2024-04-28 22:12:45 -0700

    Update module4.py

--- module4.py
+++ module4.py
@@ -1,7 +1,5 @@
 def user_input():
-	"""Input prompt"""
 
-	# Printing statement to signal the user that we are waiting for input.
 	user_input = input("Please type in your name\n")
 
 	# Printing a message based on the input.
