ed26133048bf7665aab1b5553bc13a7b82ecb0eb
wingyans <168396853+wingyans@users.noreply.github.com>
2024-04-28 23:02:21 -0700

    Update module4.py

--- module4.py
+++ module4.py
@@ -6,10 +6,29 @@
 
 #solution:
 #(1) program ask users to provide a positive integer N (used as total number of items that will be stored in the list)
-#(2) create a blank list that can hold N number of items 
+#(2) create a blank list (number list) that can hold N number of items 
 #(3) use a loop to ask user to enter the N numbers, one number each time (the number list will be updated with each number provided)
 #(4) ask the user to provide target value X which would be compared against the list of numbers that was provided earlier
 #(5) a for loop should be used in the search function to compare X against each entry in the list one by one
 #(6) if a match is identified between X and the list, the match item's index position should be returned from the function 
 #(7) if no match is found between X and the list, the function will return -1
 
+
+
+#code: 
+#matching function: 
+
+
+
+
+#ask for input main function: 
+
+def main_function():
+
+  
+
+
+
+
+#to execute the function: 
+main_function()
